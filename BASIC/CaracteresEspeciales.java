29521ca1b126647f4fcfc1f46c1f885a631ffa03
damaroja <ildefonsoarrecife3@hotmail.com>
2024-04-27 18:36:40 +0100

    Create CaracteresEspeciales.java

---
+++ BASIC/CaracteresEspeciales.java
@@ -0,0 +1,17 @@
+
+
+
+//Caracteres especiales
+
+public class CaracteresEspeciales {
+    public static void main(String args[]) {
+        System.out.println("Nueva Linea \n" + "ilde");  //Nueva linea
+        System.out.println("Tabulador \t" + "ilde");  //Tabulador
+        System.out.println("Retorno de Carro \r" + "ilde");  //Retorno de carro
+        System.out.println("Barra invertida \\" + "ilde");  //Barra invertida
+        System.out.println("Comilla simple \'" + "ilde");  //Comilla simple
+        System.out.println("Comilla doble \"" + "ilde");  //Comilla doble
+        System.out.println("Comilla simple \'" + "ilde");  //Comilla simple
+        System.out.println("Retroceso \b" + "ilde");  //Retroceso
+    }
+}
