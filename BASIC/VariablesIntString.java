74642f56aefcb91841ceda0c551bedf162ad5888
damaroja <ildefonsoarrecife3@hotmail.com>
2024-04-27 18:08:37 +0100

    Create VariablesIntString.java

---
+++ BASIC/VariablesIntString.java
@@ -0,0 +1,14 @@
+
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        int v1 = 10;
+        int v2 = 20;
+        System.out.println(v1 * v2);
+        String nombre = "Ildefonso";
+        System.out.println(nombre);
+        nombre = "Arevalo";
+        System.out.println(nombre);
+    }
+}
