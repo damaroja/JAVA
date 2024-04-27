69256a76b30b4a74e6f325a536ca8cbdff6e4710
damaroja <ildefonsoarrecife3@hotmail.com>
2024-04-27 19:01:44 +0100

    Create byte.java

---
+++ TIPOS/byte.java
@@ -0,0 +1,20 @@
+
+
+//Tipos primitivos en java
+
+public class Main {
+    public static void main(String args[]) {
+        /* 
+         * Tipos primitivos enteros
+         * byte, short, int, long, boolean, char
+         * float, double son flotantes
+         */
+
+        //Byte
+        byte b = 12;
+        System.out.println(b);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+}
+}
