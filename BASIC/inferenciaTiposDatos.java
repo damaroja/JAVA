0226fd853f64104e020dce0bda8d547df4698d74
damaroja <ildefonsoarrecife3@hotmail.com>
2024-04-27 18:14:24 +0100

    Create inferenciaTiposDatos.java

---
+++ BASIC/inferenciaTiposDatos.java
@@ -0,0 +1,11 @@
+
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        //Inferencia de tipos en java
+        //Usamos var para inferir el tipo de dato de una variable
+        var miCadena = "Hola mundo";
+        System.out.println(miCadena);
+    }
+}
